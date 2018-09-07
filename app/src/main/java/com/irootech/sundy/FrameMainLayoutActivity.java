417aeb5eb33c5407939722e251cb89b5d9bc4c0f
蒋建伟 <sundy.jiang@irootech.com>
2018-09-07 13:57:54 +0800

    提交代码

---
+++ app/src/main/java/com/irootech/sundy/FrameMainLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.irootech.sundy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameMainLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_main_layout);
+    }
+}
