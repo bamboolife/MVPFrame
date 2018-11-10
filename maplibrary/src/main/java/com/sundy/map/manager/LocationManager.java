788fc8b997a7af92528ce30921657d6274c707f7
sundy <sundy.jiang@powervision.me>
2018-11-11 01:32:57 +0800

    添加定位管理类

--- maplibrary/src/main/java/com/sundy/map/manager/LocationManager.java
+++ maplibrary/src/main/java/com/sundy/map/manager/LocationManager.java
@@ -1,8 +1,82 @@
 package com.sundy.map.manager;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
 /**
  * 定位管理类
  */
-public class LocationManager {
+public class LocationManager implements AMapLocationListener {
+    //声明mLocationOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    public AMapLocationClient mlocationClient = null;
+    private Context mContext;
 
+    public LocationManager(Context mContext) {
+        this.mContext = mContext;
+        initLocation();
+    }
+
+    /**
+     * 初始化地图定位
+     */
+    private void initLocation() {
+        mlocationClient = new AMapLocationClient(mContext);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位监听
+        mlocationClient.setLocationListener(this);
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        mLocationOption.setOnceLocationLatest(true);
+        //下面这句就是设置只定位一次的代码
+        mLocationOption.setOnceLocation(true);
+        //设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+
+    }
+
+    /**
+     * 开始定位
+     *
+     * @param
+     */
+    public void startLocation() {
+        mlocationClient.startLocation();
+    }
+
+    /**
+     * 结束定位
+     *
+     * @param
+     */
+    public void stopLocation() {
+        mlocationClient.stopLocation();
+    }
+
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                double latitude = aMapLocation.getLatitude();//获取纬度
+                double longitude = aMapLocation.getLongitude();//获取经度
+                String address=aMapLocation.getAddress();//获取地址
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"+ aMapLocation.getErrorCode() + ", errInfo:" + aMapLocation.getErrorInfo());
+            }
+        }
+    }
 }
